3c28fd93c612651a19ec0045ff4cdcd706d97b3d
houzhiyuan <834571627@q.com>
2017-12-04 20:03:13 +0800

    no message

--- api/routes/home.js
+++ api/routes/home.js
@@ -1,6 +1,7 @@
 import * as homeController from '../../controller/homeController.js'
 
 export default async (router) => {
+	homeController.initUser()
 	router.get('/login', homeController.getHomeData)
 	router.get('/insertUser', homeController.initUser)
 }
